ab03ce571e036c42b7cfac5dc44cd14e878613d5
Vanchichi <vanchgova.dasha@gmail.com>
2023-06-19 22:49:02 +0300

    Первое задание (добавлен модуль)

--- Task1/src/test/java/GreeterTest.java
+++ Task1/src/test/java/GreeterTest.java
@@ -2,8 +2,9 @@ import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.*;
 
 import hello.Greeter;
+
 import org.junit.Test;
-///
+
 public class GreeterTest {
 
     private Greeter greeter = new Greeter();
